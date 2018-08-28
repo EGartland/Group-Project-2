9c09721303145add5a77931c1c00f76bf1324e74
DefiledSpec <defiledspec@gmail.com>
2018-08-27 19:14:01 -0500

    created order and orders page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,19 @@ let db = require('../models');
 module.exports = function(app) {
   // Load index page
   app.get('/', function(req, res) {
-    db.CoffeeOrder.findAll({}).then(function(orders) {
-      res.render('index', {
-        orders
-      });
-    });
+    res.render('index');
   });
 
-  // Load example page and pass in an example by id
-  app.get('/example/:id', function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render('example', {
-        example: dbExample
-      });
-    });
+  app.get('/orders/create', (req, res) => {
+    res.render('createOrder');
   });
+
+  app.get('/orders', (req, res) => {
+    db.CoffeeOrder.findAll({}).then(function(orders) {
+      res.render('orders', { orders });
+    });    
+  });
+
 
   // Render 404 page for any unmatched routes
   app.get('*', function(req, res) {
