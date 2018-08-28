222dc0c1037ac8713ba0a3b41a769930014faddc
DefiledSpec <defiledspec@gmail.com>
2018-08-27 20:20:44 -0500

    test

--- public/js/index.js
+++ public/js/index.js
@@ -1,85 +1,30 @@
 // Get references to page elements
-var $exampleText = $('#example-text');
-var $exampleDescription = $('#example-description');
-var $submitBtn = $('#submit');
-var $exampleList = $('#example-list');
+let $orders = $('#order-list');
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveOrder: function(order) {
     return $.ajax({
       headers: {
         'Content-Type': 'application/json'
       },
       type: 'POST',
-      url: 'api/examples',
-      data: JSON.stringify(example)
+      url: 'api/orders',
+      data: JSON.stringify(order)
     });
   },
-  getExamples: function() {
+  getOrders: function() {
     return $.ajax({
-      url: 'api/examples',
+      url: 'api/orders',
       type: 'GET'
     });
   },
-  deleteExample: function(id) {
+  deleteOrder: function(id) {
     return $.ajax({
-      url: 'api/examples/' + id,
+      url: 'api/orders/' + id,
       type: 'DELETE'
     });
   }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $('<a>')
-        .text(example.text)
-        .attr('href', '/example/' + example.id);
-
-      var $li = $('<li>')
-        .attr({
-          class: 'list-group-item',
-          'data-id': example.id
-        })
-        .append($a);
-
-      var $button = $('<button>')
-        .addClass('btn btn-danger float-right delete')
-        .text('ｘ');
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert('You must enter an example text and description!');
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val('');
-  $exampleDescription.val('');
 };
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
@@ -89,11 +34,10 @@ var handleDeleteBtnClick = function() {
     .parent()
     .attr('data-id');
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
+  API.deleteOrder(idToDelete).then(function() {
+    location.reload();
   });
 };
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on('click', handleFormSubmit);
-$exampleList.on('click', '.delete', handleDeleteBtnClick);
+$orders.on('click', '.delete', handleDeleteBtnClick);
